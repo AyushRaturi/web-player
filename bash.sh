76aaaab5a816fb2c187805fcb445ca39b144b027
Ayush Raturi <ayushraturi6679@gmail.com>
2026-03-11 19:02:25 +0530

    updating bash.sh

--- bash.sh
+++ bash.sh
@@ -1,8 +1,27 @@
-# checking docker version
+#!/bin/bash
+
+# Update system
+sudo apt-get update -y
+
+# Check and install git
+if ! command -v git &> /dev/null; then
+    sudo apt-get install git -y
+fi
+git --version
+
+# Clone repository (if needed)
+# git clone https://github.com/AyushRaturi/Web-player.git
+
+# Check and install docker
+if ! command -v docker &> /dev/null; then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    rm get-docker.sh
+fi
 docker --version
 
-# build the docker image
+# Build docker image
 docker build . -t web-player
 
-# run the docker image
+# Run docker image
 docker run -p 6969:80 -d web-player
